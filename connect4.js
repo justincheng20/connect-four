e28edb7b1fd310287b3e7bba540d3bfc00172b7a
David Sommers <davidcsommers1@gmail.com>
2020-01-09 14:24:37 -0800

    Fixed rendering issue where endgame alert was happening before final piece was rendered.

--- connect4.js
+++ connect4.js
@@ -9,9 +9,10 @@ const WIDTH = 7;
 const HEIGHT = 6;
 const MAX_TURNS = WIDTH * HEIGHT;
 let turns = 0;
+let clicksDisabled = false;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
@@ -28,24 +29,24 @@ function makeBoard() {
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+  // TODO: get "htmlBoard" letiable from the item in HTML w/ID of "board"
   let htmlBoard = document.getElementById("board");
   // TODO: add comment for this code. This appends a cell to our column and adds event listeners to respond to clicks.
-  var top = document.createElement("tr");
+  let top = document.createElement("tr");
   top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
 
-  for (var x = 0; x < WIDTH; x++) {
-    var headCell = document.createElement("td");
+  for (let x = 0; x < WIDTH; x++) {
+    let headCell = document.createElement("td");
     headCell.setAttribute("id", x);
     top.append(headCell);
   }
   htmlBoard.append(top);
 
   // TODO: add comment for this code. This appends rows to each of our columns.
-  for (var y = 0; y < HEIGHT; y++) {
+  for (let y = 0; y < HEIGHT; y++) {
     const row = document.createElement("tr");
-    for (var x = 0; x < WIDTH; x++) {
+    for (let x = 0; x < WIDTH; x++) {
       const cell = document.createElement("td");
       cell.setAttribute("id", `${y}-${x}`);
       row.append(cell);
@@ -81,35 +82,43 @@ function endGame(msg) {
 /** handleClick: handle click of column top to play piece */
 
 function handleClick(evt) {
+  if(clicksDisabled) return;
+
   // get x from ID of clicked cell
-  var x = +evt.target.id;
+  let x = +evt.target.id;
 
   // get next spot in column (if none, ignore click)
-  //var y = findSpotForCol(x);
+  //let y = findSpotForCol(x);
   //if (y === null) {
   //  return;
   //}
-  var y = findSpotForCol(x);
+  let y = findSpotForCol(x);
   if (y === -1) return; 
   board[x].push(currPlayer);
   // place piece in board and add to HTML table
   // TODO: add line to update in-memory board
   placeInTable(y, x);
+  clicksDisabled = true;
   turns++;
-  // check for win
-  if (checkForWin()) {
-    return endGame(`Player ${currPlayer} won!`);
-  }
+  setTimeout(function() {
+    // check for win
+    if (checkForWin()) {
+      return endGame(`Player ${currPlayer} won!`);
+    }
 
-  // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
-  if (turns === MAX_TURNS){
-    endGame("Tie!");
-  }
-  
-  // switch players
-  // TODO: switch currPlayer 1 <-> 2
-  currPlayer = ((currPlayer === 1) ? 2 : 1);
+    // check for tie
+    // TODO: check if all cells in board are filled; if so call, call endGame
+    if (turns === MAX_TURNS){
+      endGame("Tie!");
+    }
+    
+    // switch players
+    // TODO: switch currPlayer 1 <-> 2
+    currPlayer = ((currPlayer === 1) ? 2 : 1);
+
+    clicksDisabled = false;
+  }, 100);
+
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
@@ -132,12 +141,12 @@ function checkForWin() {
 
   // TODO: read and understand this code. Add comments to help you.
   // This function creates arrays for every single possible 4-space combination of pieces in every direction. If any one of these 4 is true, we return true. 
-  for (var y = 0; y < HEIGHT; y++) {
-    for (var x = 0; x < WIDTH; x++) {
-      var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-      var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-      var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+  for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x < WIDTH; x++) {
+      let horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+      let vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+      let diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+      let diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
 
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
